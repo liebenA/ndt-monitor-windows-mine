6e9f616eb9168108923a7ae84d11dc2a080d6c3c
liebenA <liebenahs@gmail.com>
2024-09-09 19:54:50 +0100

    Add ndt7-client monitor for windows

---
+++ ndt7-client-windows/ndt7ClientExecution-Test-3min.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+rem Variables
+set "sourceDir=%~dp0"
+set "targetDir=C:\ndt7Client"
+set "exeFile=ndt7-client.exe"
+set "batFile=%~nx0"
+set "vbsFile=%targetDir%\temp.vbs"
+set "logFile=%targetDir%\execution.log"
+
+rem Création du répertoire C:\ndt7Client
+if not exist "%targetDir%" (
+    mkdir "%targetDir%"
+)
+
+rem Copie du fichier ndt7-client.exe vers C:\ndt7Client
+copy "%sourceDir%%exeFile%" "%targetDir%\"
+
+rem Copie du fichier batch vers C:\ndt7Client
+copy "%sourceDir%%batFile%" "%targetDir%\"
+
+rem Création du script VBScript pour exécuter la commande sans afficher de terminal et pour écrire dans un fichier de log
+echo Set objShell = CreateObject("WScript.Shell") > "%vbsFile%"
+echo objShell.Run "%targetDir%\%exeFile% -server monitor.uac.bj:4444" , 0, False >> "%vbsFile%"
+
+rem Création de la tâche planifiée pour exécution toutes les 3 minutes avec privilèges élevés et utilisateur SYSTEM
+SCHTASKS /CREATE /SC MINUTE /MO 3 /TN "ndt7ClientExecution-Test-3min" /TR "wscript.exe \"%vbsFile%\"" /RU "SYSTEM" /RL "HIGHEST"
+
+powershell -Command "Set-ScheduledTask -TaskName 'ndt7ClientExecution-Test-3min' -Settings (New-ScheduledTaskSettingsSet -StartWhenAvailable -DontStopIfGoingOnBatteries -AllowStartIfOnBatteries -DontStopOnIdleEnd)"
+
